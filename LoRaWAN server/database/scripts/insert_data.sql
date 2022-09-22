b7127c71bca53dbb2d7e866bcc12f76a890f9101
ParkYoonHyeong <62279820+ParkYoonHyeong@users.noreply.github.com>
2022-09-22 19:28:21 +0900

    lorawan project

---
+++ LoRaWAN server/database/scripts/insert_data.sql
@@ -0,0 +1,6 @@
+insert into parking_lot (building_number, parking_lot_number, occupied) values (311, 1, 1);
+insert into parking_lot (building_number, parking_lot_number, occupied) values (311, 2, 0);
+insert into parking_lot (building_number, parking_lot_number, occupied) values (312, 1, 1);
+insert into parking_lot (building_number, parking_lot_number, occupied) values (312, 2, 0);
+insert into parking_lot (building_number, parking_lot_number, occupied) values (313, 1, 0);
+insert into parking_lot (building_number, parking_lot_number, occupied) values (313, 2, 1);
