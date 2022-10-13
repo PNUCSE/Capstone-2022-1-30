8dabb0bfb7b3a47a9439a75a562e5cebbe8e4fe5
ParkYoonHyeong <62279820+ParkYoonHyeong@users.noreply.github.com>
2022-10-14 00:28:36 +0900

    정리

--- LoRaWAN server/database/scripts/insert_data.sql
+++ LoRaWAN server/database/scripts/insert_data.sql
@@ -4,3 +4,4 @@ insert into parking_lot (building_number, parking_lot_number, occupied) values (
 insert into parking_lot (building_number, parking_lot_number, occupied) values (312, 2, 0);
 insert into parking_lot (building_number, parking_lot_number, occupied) values (313, 1, 0);
 insert into parking_lot (building_number, parking_lot_number, occupied) values (313, 2, 1);
+insert into parking_lot (building_number, parking_lot_number, occupied) values (313, 3, 1);
